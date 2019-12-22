15941ca134696cf39d22f0ecc1c92999c7005953
veliya748 <veliyasavova@gmail.com>
2019-12-22 15:09:33 +0100

    Updates

---
+++ precache-manifest.1032c00ce93576d98faf80b6f8b48e31.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5179bec3a4d4357f16b762d438baba",
+    "url": "/practice_miller/index.html"
+  },
+  {
+    "revision": "320f37b24375cb0bf8c7",
+    "url": "/practice_miller/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "11c659bc832c93a88900",
+    "url": "/practice_miller/static/js/2.dfccb3fb.chunk.js"
+  },
+  {
+    "revision": "a0560e08c2bea09cecbe463a56dcf52b",
+    "url": "/practice_miller/static/js/2.dfccb3fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "320f37b24375cb0bf8c7",
+    "url": "/practice_miller/static/js/main.8e4084a0.chunk.js"
+  },
+  {
+    "revision": "e6c4c7bba1a5ade7fc44",
+    "url": "/practice_miller/static/js/runtime-main.347ab4dc.js"
+  },
+  {
+    "revision": "de578f1b6cbbb6ff9ae136a6cf0000b3",
+    "url": "/practice_miller/static/media/arzt.de578f1b.png"
+  },
+  {
+    "revision": "657ddffa38e0dbab85f6861e8ef448b9",
+    "url": "/practice_miller/static/media/doctor comp.657ddffa.png"
+  },
+  {
+    "revision": "3f35d1394483ca050ff7cbf34b177359",
+    "url": "/practice_miller/static/media/healthcare.3f35d139.png"
+  },
+  {
+    "revision": "ab92270c1b2f8c974693e51657af06c0",
+    "url": "/practice_miller/static/media/kaira.ab92270c.png"
+  },
+  {
+    "revision": "692727752824ed1f880389f1bbc5fd44",
+    "url": "/practice_miller/static/media/logo.69272775.png"
+  },
+  {
+    "revision": "b34c85bdeb564a07942c3bc651886f2c",
+    "url": "/practice_miller/static/media/map.b34c85bd.PNG"
+  },
+  {
+    "revision": "9c84c66ca647ca56c7a35f09efb55aca",
+    "url": "/practice_miller/static/media/office.9c84c66c.jpg"
+  },
+  {
+    "revision": "752a77e5752457fba3b1e577991bafcc",
+    "url": "/practice_miller/static/media/welcome.752a77e5.jpg"
+  }
+]);
